ae034e26194cfd275ed051d3a0f1edf8decb10e2
preethi54 <89222337+preethi54@users.noreply.github.com>
2022-11-21 16:05:36 -0600

    InitialCommit
    Commit

---
+++ SmartParking-GDP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartParking-GDP
+//
+//  Created by Jadhav,Preethi Eshwarlal on 11/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
